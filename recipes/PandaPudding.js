16c535339ff2b8c73f275f91e0436d5fd5ccb882
lcheng-pentaho <lcheng@pentaho.com>
2012-07-17 10:37:45 -0400

    Adding PandaPudding
    It's good!

---
+++ recipes/PandaPudding.js
@@ -0,0 +1,15 @@
+pentahoRecipeBook.recipes.push({
+  author: "ThePanda", 
+  title: "Panda Pudding", 
+  description: "Yummy!",
+  ingredients: [
+    {quantity: '4 cups', name: 'chocolate pudding'},
+    {quantity: '4 cups', name: 'vanilla pudding'},
+    {quantity: '1', name: 'pie tin'}
+  ],
+  steps: [
+    "Make the pudding",
+    "Swirl it in the pie tin, in a panda pattern",
+    "Put it in the fridge. Serve cold."
+  ]
+});
