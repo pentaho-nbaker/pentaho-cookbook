d3aee17d5c56ece6c553dbb28e5feae50cba6fa6
Rob Fellows <rfellows@pentaho.com>
2012-07-17 10:32:57 -0400

    Adding a recipe for the one-and-only Beer Can Chicken

---
+++ recipes/beer-can-chicken.js
@@ -0,0 +1,20 @@
+pentahoRecipeBook.recipes.push({
+  author: "Rob Fellows", 
+  title: "Beer Can Chicken", 
+  description: "Show off by cooking a chicken vertically on a beer can",
+  ingredients: [
+    {quantity: '1', name: 'whole chicken (3-4 lbs)'},
+    {quantity: '3 tbsp', name: 'favorite bbq rub'},
+    {quantity: '1 can', name: 'beer, none of that light stuff!'}
+  ],
+  steps: [
+    "Rub the chicken with the bbq rub, outside, inside, and under the skin. Cover and let it rest in the fridge for an hour.",
+    "Set up grill for indirect grilling",
+    "Open the beer can, drink half of it. This is important, you can't MUST drink beer when grilling!",
+    "Poke some more holes in the top of the beer can to allow more steam to escape when grilling.",
+    "Add any left over bbq rub to the beer, it might fizz alot don't worry.",
+    "Put the chicken over the beer can, stand it up on the grill.",
+    "Tuck the wings behind the bird",
+    "Cook for about an hour, or until the internal temp reaches 180 degrees."
+  ]
+});
