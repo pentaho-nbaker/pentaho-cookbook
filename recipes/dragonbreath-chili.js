9fef5d7ecb25982be169a874212cb2f513359ba7
Jordan Ganoff <jganoff@pentaho.com>
2012-07-17 10:45:19 -0400

    Added dragonbreath chili recipe

---
+++ recipes/dragonbreath-chili.js
@@ -0,0 +1,28 @@
+pentahoRecipeBook.recipes.push({
+  author: "Shelbi @ www.thebronzekettle.com/recipe-dragonbreath-chili", 
+  title: "Dragonbreath Chili", 
+  description: "This chili is VERY spicy. The serrano peppers range from 10,000 – 23,000 Scoville units. This is a great deal hotter than the measly jalepeño, which ranks in at 2500 – 5000 Scoville units. The real kicker is the Huy Fong sauce – made with a variety of peppers and spices.",
+  ingredients: [
+  {quantity: '2lbs', name: 'lean ground beef'},
+  {quantity: '1', name: 'serrano pepper'},
+  {quantity: '1 14oz can', name: 'beef broth'},
+  {quantity: '4oz', name: 'tomato sauce'},
+  {quantity: '2 1/2 cups', name: 'water'},
+  {quantity: '1 teaspoon', name: 'cayanne pepper'},
+  {quantity: '2 tablespoons', name: 'onion powder'},
+  {quantity: '2 tablespoons', name: 'paprika'},
+  {quantity: '1 tablespoon', name: 'cumin powder'},
+  {quantity: '5 tablespoons', name: 'chili powder'},
+  {quantity: '1 jar (8 oz)', name: 'Huy Fong Chili Garlic Sauce'}
+  ],
+  steps: [
+    "To Make Pepper Juice: Chop the pepper and boil it in a couple of ounces of water. Strain and set aside.",
+    "Brown ground beef in a large skillet or pot. Drain if necessary.",
+    "While the meat is cooking, make the pepper juice.",
+    "Once the meat is browned, add cayenne pepper, onion powder, paprika, beef broth, pepper juice, tomato sauce and water.",
+    "Cook over medium heat for 1 hour.",
+    "Add chili powder, cumin and Huy Fong Chili Garlic Sauce.",
+    "Cook an additional 15 minutes over medium heat.",
+    "Serve hot."
+  ]
+});
