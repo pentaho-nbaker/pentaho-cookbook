100a4d67a70a322f99e0d6a6710b245f2e8415a2
Benny Chow <Bchow@pentaho.com>
2013-01-14 16:25:14 +0800

    New hungryman recipe

---
+++ recipes/hungryman.js
@@ -0,0 +1,14 @@
+pentahoRecipeBook.recipes.push({
+  author: "Benny", 
+  title: "Chicken Pot Pie", 
+  description: "Impress your special someone on that special day with this romantic dinner for two",
+  ingredients: [
+    {quantity: '1', name: 'Microwave'},
+    {quantity: '2', name: 'Hungry Man Chicken Pot Pie'}
+  ],
+  steps: [
+    "Remove chicken pot pies from boxes",
+    "Cut heart shaped slits on top of each pie",
+    "Heat in microwave on High setting for 5 minutes each"
+  ]
+});
